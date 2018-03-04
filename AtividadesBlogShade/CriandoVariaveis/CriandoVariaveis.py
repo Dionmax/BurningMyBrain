4bd5f4a8555a8481fa704c533cdcd6c4ef237d7f
Dionmax Nobre <dionmax@edu.univali.br>
2018-03-04 16:37:37 -0300

    RefazendoAsCoisas

---
+++ AtividadesBlogShade/CriandoVariaveis/CriandoVariaveis.py
@@ -0,0 +1,15 @@
+#Começo Programa
+A = 5
+B = 6
+C = 4.2
+
+resultado_Um = A + B
+
+resultado_Dois = A + C * B
+
+print("Resultado um ", resultado_Um)
+print("Resultado dois ", resultado_Dois)
+
+algo = input("Teste:Escreva Algo: ")
+print(algo)
+
