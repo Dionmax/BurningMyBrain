fe3105e1354f9eea8eaa525a69805f9fddc2b171
Dionmax Nobre <dionmax@edu.univali.br>
2018-03-07 02:49:03 -0300

    TentativasFrunstadasa

---
+++ AtividadesBlogShade/CriandoVariaveis/DesviosCondicionais.py
@@ -0,0 +1,13 @@
+notasAlunos = int (input ("Digite as notas dos alunos: "))
+
+if notasAlunos >= 7:
+    print ("Aluno aprovado!")
+else:
+    print("Aluno reprovado!")
+
+#OUU
+
+print ("Aluno aprovado!!" if notasAlunos >= 7 else "Aluno reprovado!!")
+
+
+
---
+++ BlogSchadeDesviosCondicionais/BlogSchadeDesviosCondicionais/BlogSchadeDesviosCondicionais.py
@@ -0,0 +1,6 @@
+for indice in range(3):
+    notas[3] = [0]
+    notas[indice] = input()
+
+    for notas in notas:
+        print (notas)
