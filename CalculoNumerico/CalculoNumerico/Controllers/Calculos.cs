934e48d0392958f9c792c9606638a07c6152e7b8
Dionmax Nobre <dionmaxfn@gmail.com>
2018-10-10 19:55:09 -0300

    Up

---
+++ CalculoNumerico/CalculoNumerico/Controllers/Calculos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CalculoNumerico.Controllers
+{
+    public class Calculos : Controller
+    {
+       
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Comecando(string nome, int numerosCalculos = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Ola {nome}, Numero Calculos: {numerosCalculos}");
+        }
+    }
+}
