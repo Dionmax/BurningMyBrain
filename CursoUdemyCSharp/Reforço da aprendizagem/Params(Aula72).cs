c3662f29e9210ae9d1965e8f9a6a10034e9ac480
Dionmax F. Nobre <dionmaxfn@outlook.com>
2019-06-16 15:22:33 -0300

    Aulas

--- CursoUdemyCSharp/Reforço da aprendizagem/Params(Aula72).cs
+++ CursoUdemyCSharp/Reforço da aprendizagem/Params(Aula72).cs
@@ -1,11 +1,17 @@
-class TestParams
+public class Program
 {
-    public class TestParams
+    public static void Main()
     {
-        private double sum = 0;
+        System.Console.WriteLine(TestParams.somador(2, 3, 4, 5, 6));
+    }
+
+    class TestParams
+    {
+        private static int sum = 0;
+
         public static int somador(params int[] number)
         {
-            for (int i = 0; i < number.length; i++)
+            for (int i = 0; i < number.Length; i++)
             {
                 sum += number[i];
             }
