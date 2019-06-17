ca8fd70afe037ceddde4436b4b7bac7ed137adb4
Dionmax F. Nobre <dionmaxfn@outlook.com>
2019-06-16 23:52:37 -0300

    Aulas

---
+++ CursoUdemyCSharp/Aula78/Employees.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula78 {
+
+    class Employees {
+
+        public int id;
+        private string _name;
+        public float salario;
+
+        public Employees() { }
+        public Employees(int id, string name) { setName(name); setId(id); }
+        public Employees(int id, string name, float salario) : this() { setSalario(salario); }
+
+        private void setId(int id)
+            => this.id = id;
+
+        private void setName(string name)
+            => _name = name;
+
+        private void setSalario(float salario)
+            => this.salario = salario;
+
+        public void aumentarSalario(float aumento)
+            => salario += salario * aumento / 100;
+
+        public override string ToString() {
+            return "\nId: " + id + "\nNome: " + _name + "\nSalario: " + salario;
+        }
+    }
+}
---
+++ CursoUdemyCSharp/Aula78/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula78 {
+    class Program {
+        static void Main(string[] args) {
+            List<Employees> employee = new List<Employees>();
+
+            Console.WriteLine("Quantos funcionarios será registrado?");
+            int contador = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < contador; i++) {
+                Console.WriteLine("Id do funcionario: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nome do funcionário: ");
+                string name = (Console.ReadLine());
+                Console.WriteLine("Salario do mesmo: ");
+                float salario = float.Parse(Console.ReadLine());
+
+                employee.Add(new Employees(id, name, salario));
+            }
+
+            foreach (var item in employee) {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Id do funcionario que terá o salario aumento: ");
+            int IdBusca = int.Parse(Console.ReadLine());
+
+            // A função find busca o id da lista.
+            Employees funcionarioTemp = employee.Find(x => x.id == IdBusca);
+
+            if (funcionarioTemp != null) {
+                Console.WriteLine("Digite a porcentagem que vai aumentar: ");
+                float aumento = float.Parse(Console.ReadLine());
+
+                funcionarioTemp.aumentarSalario(aumento);
+            }
+            else {
+                Console.WriteLine("Id não existe.");
+            }
+
+            foreach (var item in employee) {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
