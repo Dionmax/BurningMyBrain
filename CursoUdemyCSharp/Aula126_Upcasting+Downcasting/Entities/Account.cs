c34e8890a997d3cda4a42f57d4d519b10b4d89da
Dionmax Nobre <dionmax@edu.univali.br>
2019-07-09 17:07:17 -0300

    Fim ex 126

---
+++ CursoUdemyCSharp/Aula126_Upcasting+Downcasting/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula126_Upcasting_Downcasting.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+
+        public string Holder { get; private set; }
+
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount < Balance)
+                Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+                Balance += amount;
+        }
+    }
+}
---
+++ CursoUdemyCSharp/Aula126_Upcasting+Downcasting/Program.cs
@@ -0,0 +1,22 @@
+﻿using Aula126_Upcasting_Downcasting.Entities;
+
+namespace Aula126_Upcasting_Downcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new BusinessAccount();
+            Account acc2 = new SavingsAccount();
+            Account acc3;
+
+            acc3 = acc1 as SavingsAccount;
+
+
+            if (acc2 is BusinessAccount)
+                System.Console.WriteLine("Business Account");
+            else
+                System.Console.WriteLine("Not business Account");
+        }
+    }
+}
