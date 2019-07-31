0b4fe57ced38addac38736768d5ec6608e60462e
Dionmax Nobre <dionmax@edu.univali.br>
2019-07-31 01:00:30 -0300

    Retomando Estudos (Update Interfaces)

---
+++ CursoUdemyCSharp/Aula196_Interfaces/Services/ContractService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Aula196_Interfaces.Entities;
+
+namespace Aula196_Interfaces.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePayment;
+
+        public ContractService(IOnlinePaymentService onlinePayment)
+        {
+            _onlinePayment = onlinePayment;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+
+            for (int mounthCount = 1; mounthCount <= months; mounthCount++)
+            {
+                DateTime date = contract.Date.AddMonths(mounthCount);
+
+                double updateQuota = basicQuota + _onlinePayment.Interest(basicQuota, mounthCount);
+
+                double fullQuota = updateQuota + _onlinePayment.PaymentFee(updateQuota);
+
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
---
+++ CursoUdemyCSharp/Aula196_Interfaces/Services/PaypalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula196_Interfaces.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double FEE_PERCENTAGE = 0.02;
+
+        private const double MONTHLY_INTEREST = 0.01;
+
+        public double Interest(double amount, int months)
+            => amount * MONTHLY_INTEREST * months;
+
+        public double PaymentFee(double amount)
+            => amount * FEE_PERCENTAGE;
+    }
+}
