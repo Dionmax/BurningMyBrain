898ac26b9a426cb0968f9d40b928015b865fb2de
Dionmax Nobre <dionmax@edu.univali.br>
2019-08-03 20:59:16 -0300

    Aual 202 Extensions Methods

---
+++ CursoUdemyCSharp/Aula202_ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    static class DateTimeExtensions
+    {
+        static public string ElipsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (DateTime.Now.Subtract(thisObj).TotalHours < 24.0)
+                return duration.TotalHours.ToString("F1") + " Hours";
+
+            return duration.TotalDays.ToString("F1") + " Days";
+        }
+    }
+}
---
+++ CursoUdemyCSharp/Aula202_ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula202_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2019, 08, 02, 8, 10, 45);
+            string st1 = "Good morning dear students!";
+
+            Console.WriteLine(dateTime.ElipsedTime());
+
+            Console.WriteLine(st1.Cut(10));
+        }
+    }
+}
