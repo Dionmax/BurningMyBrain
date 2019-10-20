50ca744f70f9c5d5fbe2b9be136db504b0c0f09d
Dionmax <dionmaxfn@gmail.com>
2019-10-20 18:45:39 -0300

    Up das aulas

---
+++ CursoUdemyJS/Aula_Fundamentos/ReturnObject.js
@@ -0,0 +1,11 @@
+function justReturn() {
+    const a = 1
+    const b = 2
+    const c = 3
+
+    return { a, b, c } // retorna um objeto com os valores..
+}
+
+let d = justReturn()
+
+console.log(d)
