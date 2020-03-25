66bd265bb105229c50f8b9b2d1e877b0157c3664
Dionmax <dionmaxfn@gmail.com>
2020-03-25 14:49:23 -0300

    Projetos

---
+++ CursoPHPBasico/ProjetoLogin/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
