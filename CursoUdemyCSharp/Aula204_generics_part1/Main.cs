3bf4425154865eea8307ebb9712200e4075fd8e1
Dionmax <dionmaxfn@gmail.com>
2022-01-29 18:35:40 -0300

    Curso C#: Fim aula 204

---
+++ CursoUdemyCSharp/Aula204_generics_part1/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula204_generics_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintServiceProgram printServiceProgram = new PrintServiceProgram();
+
+            Console.Write("How many values?: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printServiceProgram.AddValue(x);
+            }
+
+            printServiceProgram.Print();
+
+            Console.WriteLine("First: " + printServiceProgram.First());
+        }
+    }
+}
