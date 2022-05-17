ea220ad06ddd901f518dacf47912b87d362db8e5
Dionmax <dionmaxfn@gmail.com>
2022-05-17 17:29:53 -0300

    chore(): Install nodejs)

---
+++ DockerSymfonyClass/my_project/webpack.config.js
@@ -0,0 +1,10 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('js/custom', './build/js/custom.js')
+    .addStyleEntry('css/custom', ['./build/css/custom.css'])
+;
+
+module.exports = Encore.getWebpackConfig();
