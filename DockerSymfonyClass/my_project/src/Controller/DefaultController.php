e48ccde0253eef19ca7e0c3611f403d06bc1524e
Dionmax <dionmaxfn@gmail.com>
2022-05-20 14:49:56 -0300

    chore(User): new users on DB

--- DockerSymfonyClass/my_project/src/Controller/DefaultController.php
+++ DockerSymfonyClass/my_project/src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,32 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_default")
+     * @param ManagerRegistry $doctrine
+     * @return Response
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+
+        //Create names in database example
+//        $names = ['Keanu Correa', 'Unique Stull', 'Chase Millan', 'Kaylea Parnell', 'Keshawn Hester', 'Myah Jarrett', 'Naomi Fanning', 'Corrine Witt', 'Bridger Marcum', 'Briana Mortensen'];
+//
+//        $entityManager = $doctrine->getManager();
+//
+//        foreach ($names as $name) {
+//            $user = new User();
+//
+//            $user->setName($name);
+//
+//            $entityManager->persist($user);
+//        }
+//
+//        $entityManager->flush();
+
+        $users = $doctrine->getRepository(User::class)->findAll();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'users' => $users,
         ]);
     }
 }
