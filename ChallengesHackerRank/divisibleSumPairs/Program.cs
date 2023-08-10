0aa5d5cf88209c8c4bc46daaf2aa524cb37e8b43
Dionmax F. Nobre <dionmaxfn@gmail.com>
2023-08-10 14:53:09 -0300

    push challenges

---
+++ ChallengesHackerRank/divisibleSumPairs/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n - 1; j > i; j--)
+            {
+                //Console.Write(ar[i]);
+                //Console.Write(" + ");
+                //Console.Write(ar[j]);
+                //Console.Write(" % ");
+                //Console.Write(k);
+                //Console.Write(" = ");
+                //int t = ((ar[i] + ar[j]) % k);
+                //Console.Write(t);
+                //Console.WriteLine();
+
+                if ((ar[i] + ar[j]) % k == 0)
+                {
+                    sum++;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(path: Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        Console.WriteLine(result);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
