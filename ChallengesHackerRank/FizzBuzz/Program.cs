aac58ef1576891b08a9df7bb5b3f30cc5544e3c1
Dionmax F. Nobre <dionmaxfn@gmail.com>
2023-08-15 11:58:08 -0300

    Pusshing FizzBuzz challanger

---
+++ ChallengesHackerRank/FizzBuzz/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            string result = "";
+
+            if ((i % 3) == 0)
+                result += "Fizz";
+
+            if ((i % 5) == 0)
+                result += "Buzz";
+
+            if (result == "")
+                Console.WriteLine(i);
+            else
+                Console.WriteLine(result);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.fizzBuzz(n);
+    }
+}
